e37f57d118cc20611b47335fb3aa61e5a06e71fa
subh1l <164479102+subh1l@users.noreply.github.com>
2024-03-26 09:48:13 +0700

    Delete CRUD project directory

--- CRUD project/crud/__init__.py
+++
@@ -1,2 +0,0 @@
-from .database import init_terminal
-from .view import read_terminal, create_terminal, update_terminal, delete_terminal
--- CRUD project/crud/util.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-def random_string(length):
-    result = "".join(random.choice(string.ascii_letters) for i in range(length))
-    return result
