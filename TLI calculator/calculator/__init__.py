eb92567f12276f62dd3e7bf6c7b0dde83a0c4279
subh1l <iilmubarak.06@gmail.com>
2024-04-04 13:37:26 +0700

    removing calc

--- TLI calculator/calculator/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def refresh():
-        operating_system = os.name
-
-        match operating_system:
-                case "posix": return os.system("clear")
-                case "nt": return os.system("cls")
